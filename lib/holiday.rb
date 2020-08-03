3d6170c8bb3c04f957ae99838248147dfccff2df
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-03 21:53:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
-  puts "#{seaspn.capitalize}"
+  puts "#{seaspn.capitalize}: "
   holiday.each do |holiday, supply|
     puts "#{supply"
   # iterate through holiday_hash and print items such that your readout resembles:
