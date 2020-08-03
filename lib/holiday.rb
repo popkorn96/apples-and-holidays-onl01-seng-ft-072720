9c4b5508d4a97b56ac7a13c4377f70d0a89e33e4
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-03 21:27:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,8 +32,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-holiday_hash[:spring][:memorial_day].push("Grill")
-holiday_hash[:spring][:memorial_day].push("Table Cloth")
+holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
