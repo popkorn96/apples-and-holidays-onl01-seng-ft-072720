b757bf60b0cd9e8698eadc71d36eb333a8d03a6a
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-03 22:30:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,6 +70,9 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.each do |season, holiday|
   holiday.each do |holiday, supply|
     holiday if supply.include? "BBQ"
+  end 
+end.flatten.compact
+end
     
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
