805e27971d6e9d0939a2c9ccf97056192daec276
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-03 21:45:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-holiday_hash.values.flatten
+holiday_hash.each do |season, supply
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
